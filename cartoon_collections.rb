d65aed25a34bf51067418256b9f651c397d2f5c6
John Crespi Oncher <johnoncher@gmail.com>
2018-12-29 20:01:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,7 +25,7 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   binding.pry
-  if array & cheese_types == true
+  if array & cheese_types == false
     
     return array & cheese_types
   else return nil
