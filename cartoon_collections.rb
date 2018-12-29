525451d5d9c6350cb475c494eacf9e7845633086
John Crespi Oncher <johnoncher@gmail.com>
2018-12-29 19:21:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (elements)
+  elements.map do |element|
+    
 end
 
 def find_the_cheese# code an argument here
